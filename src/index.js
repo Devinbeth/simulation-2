02a1e293b00694d43fe96e83c6239748a381c072
Devin Lethbridge <devinbeth@gmail.com>
2018-02-23 10:07:25 -0700

    stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import store from './store.js';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
